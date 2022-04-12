9b704e6e491f321bdf3145d11a3ce03a48468db7
owencqueen <owencqueen@hotmail.com>
2022-04-12 11:50:16 -0400

    Some spectral embedding trials - not doing well

--- models/cluster.py
+++ models/cluster.py
@@ -1,13 +1,16 @@
-import sys; sys.path.append('/data1/compbio/oqueen/poplar/MashPredict/utils')
+import sys; sys.path.append('../utils')
+import random
 import numpy as np
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 from extract import *
 
 from sklearn.cluster import SpectralClustering
 from sklearn.neighbors import kneighbors_graph as knn_graph
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
 
 def cluster_all():
 
@@ -18,10 +21,13 @@ def cluster_all():
 
     y = y.iloc[list(np.nonzero(ymask)[0])].tolist()
 
+    yordinal = LabelEncoder().fit_transform(y)
+    print(yordinal[:5])
+
     model = SpectralClustering(
         n_clusters = 5,
         assign_labels = 'discretize',
-        metric = 'precomputed',
+        affinity = 'precomputed',
     )
 
     clustering = model.fit(dist)
@@ -29,14 +35,22 @@ def cluster_all():
     yhat = clustering.labels_
 
     # Create knn graph:
-    adj = knn_graph(dist, metric = 'precomputed')
+    adj = knn_graph(dist, 4, metric = 'precomputed')
+
+    # Randomly sample nodes:
+    random_samp = sorted(random.sample(list(np.arange(adj.shape[0])), 200))
+
+    #adj_samp = adj[random_samp,random_samp]
     
-    G = nx.from_numpy_matrix(adj)
+    G = nx.from_numpy_matrix(adj.toarray())
 
-
-
-    nx.draw(G)
+    pos = nx.draw(G, node_color = yordinal, node_size = 200)
+    #nx.draw_networkx_labels(G, pos, labels = y)
     plt.show()
+
+    # G = nx.from_numpy_matrix(adj_samp)
+    # pos = nx.draw(G, node_color = yordinal[random_samp])
+    # plt.show()
 
     # Overlay with actual labels:
 
